844d957541e21f43111bc5f459d4b5e9a2b4ea07
Pierre-Louis Barbarant <pierre-louis.barbarant@inria.fr>
2026-05-07 19:01:41 +0200

    Add support for both RT and outcome_start when creating events

--- src/exd/events.py
+++ src/exd/events.py
@@ -4,20 +4,28 @@ from pathlib import Path
 import pandas as pd
 
 
-def make_events(features: pd.DataFrame):
-    onset = features.trial_start / 1000
-    duration = (features.trial_end - features.trial_start) / 1000
+def make_events(
+    features: pd.DataFrame,
+    onset,
+):
+    if onset == "RT":
+        onset = (features.trial_start + features.RT) / 1000
+    elif onset == "outcome":
+        onset = (features.outcome_start) / 1000
+    duration = onset * 0  # Dirac event
     features = features.drop(columns=["Unnamed: 0"])
     events = pd.concat(
         [onset.rename("onset"), duration.rename("duration"), features],
         axis=1,
     )
+    # Drop rows with NaN onset
+    events = events.dropna(subset=["onset"])
     return events
 
 
-def get_run_events(derivatives_dir, sub, ses, run):
+def get_run_events(derivatives_dir, sub, ses, run, onset):
     suffix = f"/sub-{sub}/ses-{ses}/beh/sub-{sub}_ses-{ses}_task-ExplorePlus_run-{run:02d}_desc-formatted_beh.tsv"
     path_beh = str(Path(derivatives_dir)) + suffix
     features = pd.read_csv(path_beh, sep="\t", index_col=None)
-    events = make_events(features)
+    events = make_events(features, onset)
     return events
