dcc993c30625e36b75e41e260a57732b7cfc0804
Pierre-Louis Barbarant <pierre-louis.barbarant@inria.fr>
2026-06-01 14:59:18 +0200

    Update plot_fmri_motor

--- examples/plot_fmri_motor.py
+++ examples/plot_fmri_motor.py
@@ -1,37 +1,18 @@
 # %%
+from functools import partial
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from nilearn.glm.first_level import (
-    first_level_from_bids,
-    make_first_level_design_matrix,
-)
-from nilearn.plotting import (
-    plot_design_matrix,
-)
+from nilearn.glm.first_level import make_first_level_design_matrix
+from nilearn.glm.first_level.first_level import _list_valid_subjects
 from sklearn.base import clone
-from tqdm import tqdm
 
-from exd.events import get_run_events
+from exd.fmri_utils import first_level_analysis
 from exd.models.motor import MotorEstimator
 
 
-def get_subject_session_runs_beh(data_dir, subject, ses, task_label) -> set:
-    """Glob BIDS files to find available run indices for a subject/session."""
-    pattern = f"sub-{subject}/ses-{ses}/beh/*task-{task_label}*_run-*_beh.tsv"
-    run_files = sorted(Path(data_dir).glob(pattern))
-    return set([int(f.stem.split("run-")[1].split("_")[0]) for f in run_files])
-
-
-def get_fmri_sessions(derivatives_dir, subject, task_label) -> set:
-    """Get available fMRI sessions for a subject."""
-    pattern = f"sub-{subject}/ses-*/func"
-    func_dirs = sorted(Path(derivatives_dir).glob(pattern))
-    return set([int(f.parent.name.split("-")[1]) for f in func_dirs])
-
-
-def make_dmtx_one_run(model, events, confounds, estimator) -> pd.DataFrame:
+def make_dmtx_one_run_motor(model, events, confounds, estimator) -> pd.DataFrame:
     algo = clone(estimator)
     events["trial_type"] = algo.fit_predict(events)
     # Create design matrix
@@ -59,112 +40,27 @@ def make_dmtx_one_run(model, events, confounds, estimator) -> pd.DataFrame:
     return dtmx
 
 
-def first_level_analysis(
-    data_dir,
-    derivatives_folder,
-    task_label,
-    space_label,
-    subjects,
-    estimator,
-):
-    (
-        models_all_subjects,
-        run_imgs_all_subjects,
-        _,
-        confounds_all_subjects,
-    ) = first_level_from_bids(
-        data_dir,
-        task_label,
-        space_label,
-        sub_labels=subjects,
-        smoothing_fwhm=6.0,
-        high_pass=1 / 128,
-        t_r=1.71,
-        hrf_model="spm + derivative",
-        derivatives_folder=derivatives_folder,
-        confounds_strategy=["motion", "global_signal"],
-        n_jobs=10,
-        verbose=10,
-    )
-    for i, subject in tqdm(enumerate(subjects), desc="Processing subject: "):
-        model = models_all_subjects[i]
-        run_imgs = run_imgs_all_subjects[i]
-        confounds = confounds_all_subjects[i]
-
-        fmri_sessions = get_fmri_sessions(derivatives_dir, subject, task_label)
-        run_map = {
-            (
-                int(p.split("ses-")[1].split("/")[0]),
-                int(p.split("run-")[1].split("_")[0]),
-            ): (p, c)
-            for p, c in zip(run_imgs, confounds)
-        }
-
-        for ses in fmri_sessions:
-            beh_runs = get_subject_session_runs_beh(
-                derivatives_dir, subject, ses, task_label
-            )
-            # Keep only ses, runs with behavioral data attached
-            keys = sorted(k for k in run_map if k[0] == ses and k[1] in beh_runs)
-
-            selected_imgs = [run_map[k][0] for k in keys]
-            selected_confounds = [run_map[k][1] for k in keys]
-            selected_run_ids = [k[1] for k in keys]
-
-            design_matrices = []
-            for run_id, confounds in zip(selected_run_ids, selected_confounds):
-                run_events = get_run_events(
-                    derivatives_dir, sub=subject, ses=ses, run=run_id, onset="RT"
-                )
-                dmtx = make_dmtx_one_run(model, run_events, confounds, estimator)
-                design_matrices.append(dmtx)
-                plot_design_matrix(
-                    dmtx,
-                    rescale=True,
-                    output_file=f"/home/plbarbarant/repos/explore_design/outputs/sub-{subject}_ses-{ses}_run-{run_id}_dmtx_motor.png",
-                )
-
-            model.fit(run_imgs=selected_imgs, design_matrices=design_matrices)
-            beta_map = model.compute_contrast("R - L", output_type="effect_size")
-            beta_map.to_filename(
-                f"/home/plbarbarant/repos/explore_design/outputs/sub-{subject}_ses-{ses}_motor.nii.gz",
-            )
-
-
 if __name__ == "__main__":
-    derivatives_dir = Path(
-        "/home/plbarbarant/nasShare/projects/protocols/ExplorePlus_MeynielPaunovRaglio_2024/derivatives/fmriprep-24.1.1_mne-bids-pipeline-1.9.0"
+    mask_img = "data/mask.nii.gz"
+    data_dir = Path(
+        "/home/plbarbarant/nasShare/projects/protocols/ExplorePlus_MeynielPaunovRaglio_2024"
     )
-    data_dir = "/home/plbarbarant/nasShare/projects/protocols/ExplorePlus_MeynielPaunovRaglio_2024"
     task_label = "ExplorePlus"
     space_label = "MNI152NLin2009cAsym"
     derivatives_folder = "derivatives/fmriprep-24.1.1_mne-bids-pipeline-1.9.0/"
-    subjects = [
-        "01",
-        "04",
-        "05",
-        "06",
-        "08",
-        "09",
-        "10",
-        "11",
-        "13",
-        "14",
-        "15",
-        "16",
-        "17",
-        "18",
-        "19",
-        "20",
-    ]
+    subjects = _list_valid_subjects(str(data_dir / derivatives_folder), None)
     estimator = MotorEstimator(
         option_cols=["obsA", "obsB"],
     )
     first_level_analysis(
-        data_dir=data_dir,
+        data_dir=str(data_dir),
         task_label=task_label,
         space_label=space_label,
         derivatives_folder=derivatives_folder,
         subjects=subjects,
-        estimator=estimator,
+        mask_img=mask_img,
+        quantity_name="motor",
+        onset="RT",
+        contrast_name="R - L",
+        dmtx_functor=partial(make_dmtx_one_run_motor, estimator=estimator),
     )
