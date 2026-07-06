8f8635cf18f7cef38fa1cee82af0d985a7482b2a
Pierre-Louis Barbarant <pierre-louis.barbarant@inria.fr>
2026-07-06 22:42:08 +0200

    Update second level glm

--- examples/glm_second_level.py
+++ examples/glm_second_level.py
@@ -2,11 +2,12 @@
 from pathlib import Path
 
 import pandas as pd
+from nilearn.datasets import load_fsaverage
 from nilearn.glm.first_level.first_level import _list_valid_subjects
 from nilearn.glm.second_level import SecondLevelModel, make_second_level_design_matrix
-from nilearn.plotting import plot_design_matrix, plot_img_on_surf, show
+from nilearn.plotting import plot_design_matrix, plot_surf_stat_map, show
+from nilearn.surface import SurfaceImage
 
-mask_img = "data/mask.nii.gz"
 data_dir = Path(
     "/home/plbarbarant/nasShare/projects/protocols/ExplorePlus_MeynielPaunovRaglio_2024"
 )
@@ -16,22 +17,37 @@ derivatives_folder = "derivatives/fmriprep-24.1.1_mne-bids-pipeline-1.9.0/"
 subjects_label = _list_valid_subjects(str(data_dir / derivatives_folder), None)
 quantity_name = "motor"
 
-imgs = sorted(
+imgs_L = sorted(
     list(
         Path("/home/plbarbarant/repos/explore_design/outputs/").rglob(
-            f"sub-*/ses-*/quantity_{quantity_name}_stat-effect_size.nii.gz"
+            f"sub-*/ses-*/quantity_{quantity_name}_stat-effect_size_hemi-L*"
         )
     )
 )
+imgs_R = sorted(
+    list(
+        Path("/home/plbarbarant/repos/explore_design/outputs/").rglob(
+            f"sub-*/ses-*/quantity_{quantity_name}_stat-effect_size_hemi-R*"
+        )
+    )
+)
+imgs = []
+subjects_label = []
+mesh = load_fsaverage("fsaverage5")["pial"]
+for img_L, img_R in zip(imgs_L, imgs_R):
+    data = {
+        "left": img_L,
+        "right": img_R,
+    }
+    subjects_label.append("sub-" + str(img_L)[51:53])
+    imgs.append(SurfaceImage(mesh=mesh, data=data))
 
-subjects_label = ["sub-" + str(img_path)[51:53] for img_path in imgs]
+# %%
 confounds = pd.read_csv(data_dir / "rawdata" / "participants.tsv", sep="\t")[
     ["participant_id", "sex"]
 ]
 confounds.rename(columns={"participant_id": "subject_label"}, inplace=True)
 confounds["sex"] = confounds["sex"].map(lambda s: 0 if s == "M" else 1)
-confounds
-# %%
 dmtx = make_second_level_design_matrix(
     subjects_label=subjects_label,
     confounds=confounds,
@@ -40,19 +56,15 @@ plot_design_matrix(dmtx)
 show()
 # %%
 model = SecondLevelModel(
-    mask_img=mask_img,
     n_jobs=10,
     verbose=10,
 )
 model.fit(imgs, design_matrix=dmtx)
 stat_map = model.compute_contrast("intercept", output_type="z_score")
 # %%
-import matplotlib.pyplot as plt
-
-plot_img_on_surf(
-    stat_map,
-    inflate=True,
-    views=["lateral"],
+plot_surf_stat_map(
+    stat_map=stat_map,
+    hemi="both",
     # threshold=1.96,
     cmap="cold_hot",
     bg_on_data=False,
